6b3824bcfe4bc14ed03dbacfdd3a8dde53c2f2d2
Alex Jenkins <108849441+ajenkinsynwa@users.noreply.github.com>
2023-02-11 22:02:42 -0500

    creatpost

---
+++ src/page/index.js
@@ -0,0 +1,7 @@
+import Home from '../page/Home';
+import CreatePost from '../page/CreatePost';
+
+export {
+  Home,
+  CreatePost,
+};
---
+++ src/utils/index.js
@@ -0,0 +1,11 @@
+import { surpriseMePrompts } from '../constants';
+
+//excepts prompt as parameter
+export function getRandomPrompt(prompt) {
+  const randomIndex = Math.floor(Math.random() * surpriseMePrompts.length);
+  const randomPrompt = surpriseMePrompts[randomIndex];
+
+  if (randomPrompt === prompt) return getRandomPrompt(prompt);
+
+  return randomPrompt;
+}
