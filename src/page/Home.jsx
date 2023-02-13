f46c9c6b95f7c1a40c0151982fc3e3b53d4a839e
Alex Jenkins <108849441+ajenkinsynwa@users.noreply.github.com>
2023-02-12 22:41:46 -0500

    updated serverside

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -19,48 +19,47 @@ const Home = () => {
   const [allPosts, setAllPosts] = useState(null);
 
   const [searchText, setSearchText] = useState('');
-  // const [searchTimeout, setSearchTimeout] = useState(null);
-  // const [searchedResults, setSearchedResults] = useState(null);
 
-  // const fetchPosts = async () => {
-  //   setLoading(true);
+useEffect(() => {
+  const fetchPosts = async () => { 
+    setLoading(true);
 
-  //   try {
-  //     const response = await fetch('https://dalle-arbb.onrender.com/api/v1/post', {
-  //       method: 'GET',
-  //       headers: {
-  //         'Content-Type': 'application/json',
-  //       },
-  //     });
+  try {
+    const response = await fetch('http://localhost:8080/api/v1/post', {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
 
-  //     if (response.ok) {
-  //       const result = await response.json();
-  //       setAllPosts(result.data.reverse());
-  //     }
-  //   } catch (err) {
-  //     alert(err);
-  //   } finally {
-  //     setLoading(false);
-  //   }
-  // };
+if (response.ok) {
+        const result = await response.json();
+        setAllPosts(result.data.reverse());
+  }
+} catch (err) {
+  alert(err);
 
-  // useEffect(() => {
-  //   fetchPosts();
-  // }, []);
+  } finally {
+    setLoading(false);
+  }
+}
 
-  // const handleSearchChange = (e) => {
-  //   clearTimeout(searchTimeout);
-  //   setSearchText(e.target.value);
+  fetchPosts();
+}, []);
 
-  //   setSearchTimeout(
-  //     setTimeout(() => {
-  //       const searchResult = allPosts.filter((item) => item.name.toLowerCase().includes(searchText.toLowerCase()) || item.prompt.toLowerCase().includes(searchText.toLowerCase()));
-  //       setSearchedResults(searchResult);
-  //     }, 500),
-  //   );
-  // };
+const handleSearchChange = (e) => {
+  clearTimeout(searchTimeout);
+  setSearchText(e.target.value);
 
-  return (
+  setSearchTimeout(
+    setTimeout(() => {
+      const searchResult = allPosts.filter((item) => item.name.toLowerCase().includes(searchText.toLowerCase()) || item.prompt.toLowerCase().includes(searchText.toLowerCase()));
+      setSearchedResults(searchResult);
+    }, 500),
+  );
+};
+ 
+ return (
     <section className="max-w-7xl mx-auto">
       <div>
         <h1 className="font-extrabold text-[#222328] text-[32px]">The Community Showcase</h1>
@@ -98,7 +97,7 @@ const Home = () => {
                 />
               ) : (
                 <RenderCards
-                  data={[]}
+                  data={allPosts}
                   title="No Posts Yet"
                 />
               )}
