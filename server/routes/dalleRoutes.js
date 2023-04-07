1959cbc57c1460729d326d549d507344a7fdf590
Alex Jenkins <108849441+ajenkinsynwa@users.noreply.github.com>
2023-04-06 22:14:34 -0400

    Update dalleRoutes.js

--- server/routes/dalleRoutes.js
+++ server/routes/dalleRoutes.js
@@ -1,25 +1,35 @@
-import express from 'express';
-import * as dotenv from 'dotenv';
-import { Configuration, OpenAIApi } from 'openai';
+// Import required modules
+import express from 'express'; // Express.js for creating web server
+import * as dotenv from 'dotenv'; // dotenv for managing environment variables
+import { Configuration, OpenAIApi } from 'openai'; // openai package for connecting to OpenAI API
 
+// Load environment variables from .env file
 dotenv.config();
 
+// Create an instance of the Express.js router
 const router = express.Router();
 
+// Configure OpenAI API key
 const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
+  apiKey: process.env.OPENAI_API_KEY, // Store API key in environment variable
 });
 
+// Create an instance of the OpenAI API
 const openai = new OpenAIApi(configuration);
 
+// Define a route for handling GET requests
 router.route('/').get((req, res) => {
+  // Return a JSON response with a greeting message
   res.status(200).json({ message: 'Hello from DALL-E!' });
 });
 
+// Define a route for handling POST requests
 router.route('/').post(async (req, res) => {
   try {
+    // Extract the prompt string from the request body
     const { prompt } = req.body;
 
+    // Use the OpenAI API to create an image based on the prompt
     const aiResponse = await openai.createImage({
       prompt,
       n: 1,
@@ -27,12 +37,18 @@ router.route('/').post(async (req, res) => {
       response_format: 'b64_json',
     });
 
+    // Extract the base64-encoded image data from the API response
     const image = aiResponse.data.data[0].b64_json;
+
+    // Return a JSON response with the image data
     res.status(200).json({ photo: image });
   } catch (error) {
+    // Log any errors to the console
     console.error(error);
+    // Return an error message as the response
     res.status(500).send(error?.response.data.error.message || 'Something went wrong');
   }
 });
 
+// Export the router for use in other modules
 export default router;
