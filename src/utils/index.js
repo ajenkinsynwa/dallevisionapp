26a6930932e64a4774552d44d65cf91ece40ddd6
Alex Jenkins <108849441+ajenkinsynwa@users.noreply.github.com>
2023-04-09 20:09:20 -0400

    Update index.js

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,15 +1,17 @@
-import { surpriseMePrompts } from '../constants';
-import FileSaver from 'file-saver';
+import { surpriseMePrompts } from '../constants'; // Importing a list of prompts from a file located in a parent directory
+import FileSaver from 'file-saver'; // Importing the FileSaver library
 
-//excepts prompt as parameter
+// This function takes a prompt as a parameter and returns a random prompt from the list of prompts, except for the one passed as an argument
 export function getRandomPrompt(prompt) {
-  const randomIndex = Math.floor(Math.random() * surpriseMePrompts.length);
-  const randomPrompt = surpriseMePrompts[randomIndex];
+  const randomIndex = Math.floor(Math.random() * surpriseMePrompts.length); // Generating a random index within the range of the surpriseMePrompts array
+  const randomPrompt = surpriseMePrompts[randomIndex]; // Retrieving a random prompt from the array using the random index
 
-  if (randomPrompt === prompt) return getRandomPrompt(prompt);
+  if (randomPrompt === prompt) return getRandomPrompt(prompt); // If the retrieved prompt is the same as the one passed as an argument, recursively call the function until a different prompt is returned
 
-  return randomPrompt;
+  return randomPrompt; // Return the random prompt that was retrieved
 }
+
+// This function downloads an image file and saves it with a file name that includes an id in the format "download-<id>.jpg"
 export async function downloadImage(_id, photo) {
-  FileSaver.saveAs(photo, `download-${_id}.jpg`);
+  FileSaver.saveAs(photo, `download-${_id}.jpg`); // Using the FileSaver library to save the file with the specified file name
 }
